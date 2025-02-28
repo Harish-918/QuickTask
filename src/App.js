cf9e414d632a7dfd374f31b3ad7495491ea1181a
Kotakonda Govindarajulu <harish.govindarajulu@accenture.com>
2025-02-28 10:50:50 +0530

    Version 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks = localStorage.getItem('tasks');
+    return savedTasks ? JSON.parse(savedTasks) : [];
+  });
+
+  const [newTask, setNewTask] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const addTask = () => {
+    if (newTask.trim() === '') return;
+
+    const task = {
+      id: Date.now(),
+      text: newTask,
+      completed: false,
+    };
+
+    setTasks((prevTasks) => [task, ...prevTasks]);
+    setNewTask('');
+  };
+
+  const toggleTaskCompletion = (taskId) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId
+          ? { ...task, completed: !task.completed }
+          : task
+      )
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  const sortedTasks = [...tasks].sort((a, b) => {
+    if (a.completed === b.completed) {
+      return b.id - a.id;
+    }
+    return a.completed ? 1 : -1;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>QuickTask</h1>
+      <div className='input-container'>
+        <input type='text' value={newTask} onChange={(e) => setNewTask(e.target.value)} placeholder='Add Task' />
+        <button className='add-task' onClick={addTask}>Add Task</button>
+      </div>
+
+      <ul>
+        {sortedTasks.map((task) => (
+          <li key={task.id} className={task.completed ? 'completed' : ''}>
+            <span onClick={() => toggleTaskCompletion(task.id)}>{task.text}</span>
+            <button className='remove-task' onClick={() => deleteTask(task.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
